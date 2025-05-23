wwsean08/app

feat: add support for AWS

cmd/app/main.go
@@ -12,12 +12,12 @@ import (
 
 	"chainguard.dev/go-grpc-kit/pkg/duplex"
 	pboidc "chainguard.dev/sdk/proto/platform/oidc/v1"
-	kms "cloud.google.com/go/kms/apiv1"
 	"github.com/chainguard-dev/clog"
 	metrics "github.com/chainguard-dev/terraform-infra-common/pkg/httpmetrics"
 	mce "github.com/chainguard-dev/terraform-infra-common/pkg/httpmetrics/cloudevents"
 	envConfig "github.com/octo-sts/app/pkg/envconfig"
 	"github.com/octo-sts/app/pkg/ghtransport"
+	"github.com/octo-sts/app/pkg/kms"
 	"github.com/octo-sts/app/pkg/octosts"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -44,16 +44,15 @@ func main() {
 		defer metrics.SetupTracer(ctx)()
 	}
 
-	var client *kms.KeyManagementClient
-
+	var kmsClient kms.KMS
 	if baseCfg.KMSKey != "" {
-		client, err = kms.NewKeyManagementClient(ctx)
+		kmsClient, err = kms.NewKMS(ctx, baseCfg.KMSProver, baseCfg.KMSKey)
 		if err != nil {
 			log.Panicf("could not create kms client: %v", err)
 		}
 	}
 
-	atr, err := ghtransport.New(ctx, baseCfg, client)
+	atr, err := ghtransport.New(ctx, baseCfg, kmsClient)
 	if err != nil {
 		log.Panicf("error creating GitHub App transport: %v", err)
 	}

cmd/webhook/main.go
@@ -14,13 +14,13 @@ import (
 	"strings"
 	"time"
 
-	kms "cloud.google.com/go/kms/apiv1"
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 	"github.com/chainguard-dev/clog"
 	metrics "github.com/chainguard-dev/terraform-infra-common/pkg/httpmetrics"
 	envConfig "github.com/octo-sts/app/pkg/envconfig"
 	"github.com/octo-sts/app/pkg/ghtransport"
+	"github.com/octo-sts/app/pkg/kms"
 	"github.com/octo-sts/app/pkg/webhook"
 )
 
@@ -45,23 +45,23 @@ func main() {
 		defer metrics.SetupTracer(ctx)()
 	}
 
-	var client *kms.KeyManagementClient
+	var kmsClient kms.KMS
 
 	if baseCfg.KMSKey != "" {
-		client, err = kms.NewKeyManagementClient(ctx)
+		kmsClient, err = kms.NewKMS(ctx, baseCfg.KMSProver, baseCfg.KMSKey)
 		if err != nil {
 			log.Panicf("could not create kms client: %v", err)
 		}
 	}
 
-	atr, err := ghtransport.New(ctx, baseCfg, client)
+	atr, err := ghtransport.New(ctx, baseCfg, kmsClient)
 	if err != nil {
 		log.Panicf("error creating GitHub App transport: %v", err)
 	}
 
 	// Fetch webhook secrets from secret manager
 	// or allow webhook secret to be defined by env var.
-	// Not everyone is using Google KMS, so we need to support other methods
+	// Not everyone is using Google kmsProvider, so we need to support other methods
 	webhookSecrets := [][]byte{}
 	if baseCfg.KMSKey != "" {
 		secretmanager, err := secretmanager.NewClient(ctx)

pkg/envconfig/envconfig.go
@@ -12,6 +12,7 @@ import (
 type EnvConfig struct {
 	Port                       int    `envconfig:"PORT" required:"true"`
 	KMSKey                     string `envconfig:"KMS_KEY" required:"false"`
+	KMSProver                  string `envconfig:"KMS_PROVER" required:"false" default:"gcp"`
 	AppID                      int64  `envconfig:"GITHUB_APP_ID" required:"true"`
 	AppSecretCertificateFile   string `envconfig:"APP_SECRET_CERTIFICATE_FILE" required:"false"`
 	AppSecretCertificateEnvVar string `envconfig:"APP_SECRET_CERTIFICATE_ENV_VAR" required:"false"`

pkg/ghtransport/ghtransport_test.go
@@ -16,8 +16,10 @@ import (
 	"os"
 	"testing"
 
-	kms "cloud.google.com/go/kms/apiv1"
+	gcpKMS "cloud.google.com/go/kms/apiv1"
+	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/octo-sts/app/pkg/envconfig"
+	kmsGCP "github.com/octo-sts/app/pkg/gcpkms"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
@@ -34,13 +36,19 @@ func TestGCPKMS(t *testing.T) {
 	t.Setenv("GOOGLE_APPLICATION_CREDENTIALS", credsFile)
 
 	testConfig := &envconfig.EnvConfig{
-		Port:    8080,
-		AppID:   123456,
-		KMSKey:  "test-kms-key",
-		Metrics: true,
+		Port:      8080,
+		AppID:     123456,
+		KMSKey:    "test-kms-key",
+		KMSProver: "gcp",
+		Metrics:   true,
+	}
+	provider := testKMSSigner{
+		t:   t,
+		key: testConfig.KMSKey,
+		ctx: ctx,
 	}
 
-	transport, err := New(ctx, testConfig, generateKMSClient(ctx, t))
+	transport, err := New(ctx, testConfig, &provider)
 
 	assert.NoError(t, err)
 
@@ -59,7 +67,7 @@ func TestCertEnvVar(t *testing.T) {
 		Metrics:                    true,
 	}
 
-	transport, err := New(ctx, testConfig, generateKMSClient(ctx, t))
+	transport, err := New(ctx, testConfig, nil)
 
 	assert.NoError(t, err)
 
@@ -76,21 +84,21 @@ func TestCertFile(t *testing.T) {
 		Metrics:                  true,
 	}
 
-	transport, err := New(ctx, testConfig, generateKMSClient(ctx, t))
+	transport, err := New(ctx, testConfig, nil)
 
 	assert.NoError(t, err)
 
 	assert.NotNil(t, transport)
 }
 
-func generateKMSClient(ctx context.Context, t *testing.T) *kms.KeyManagementClient {
+func generateKMSClient(ctx context.Context, t *testing.T) *gcpKMS.KeyManagementClient {
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatal(err)
 	}
 	fakeServerAddr := l.Addr().String()
 
-	client, err := kms.NewKeyManagementClient(ctx,
+	client, err := gcpKMS.NewKeyManagementClient(ctx,
 		option.WithEndpoint(fakeServerAddr),
 		option.WithoutAuthentication(),
 		option.WithGRPCDialOption(grpc.WithTransportCredentials(insecure.NewCredentials())),
@@ -163,4 +171,15 @@ func generateTestCertificateFile(t *testing.T) string {
 	}
 
 	return tmpFile.Name()
+}
+
+type testKMSSigner struct {
+	t   *testing.T
+	key string
+	ctx context.Context
+}
+
+func (k *testKMSSigner) NewSigner() (ghinstallation.Signer, error) {
+	client := generateKMSClient(k.ctx, k.t)
+	return kmsGCP.New(k.ctx, client, k.key)
 }

wbd-open-source/octo-sts-app

cmd/app/main.go
@@ -12,12 +12,12 @@ import (
 
 	"chainguard.dev/go-grpc-kit/pkg/duplex"
 	pboidc "chainguard.dev/sdk/proto/platform/oidc/v1"
-	kms "cloud.google.com/go/kms/apiv1"
 	"github.com/chainguard-dev/clog"
 	metrics "github.com/chainguard-dev/terraform-infra-common/pkg/httpmetrics"
 	mce "github.com/chainguard-dev/terraform-infra-common/pkg/httpmetrics/cloudevents"
 	envConfig "github.com/octo-sts/app/pkg/envconfig"
 	"github.com/octo-sts/app/pkg/ghtransport"
+	"github.com/octo-sts/app/pkg/kms"
 	"github.com/octo-sts/app/pkg/octosts"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -44,16 +44,15 @@ func main() {
 		defer metrics.SetupTracer(ctx)()
 	}
 
-	var client *kms.KeyManagementClient
-
+	var kmsClient kms.KMS
 	if baseCfg.KMSKey != "" {
-		client, err = kms.NewKeyManagementClient(ctx)
+		kmsClient, err = kms.NewKMS(ctx, baseCfg.KMSProvider, baseCfg.KMSKey)
 		if err != nil {
 			log.Panicf("could not create kms client: %v", err)
 		}
 	}
 
-	atr, err := ghtransport.New(ctx, baseCfg, client)
+	atr, err := ghtransport.New(ctx, baseCfg, kmsClient)
 	if err != nil {
 		log.Panicf("error creating GitHub App transport: %v", err)
 	}

cmd/webhook/main.go
@@ -14,13 +14,13 @@ import (
 	"strings"
 	"time"
 
-	kms "cloud.google.com/go/kms/apiv1"
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 	"github.com/chainguard-dev/clog"
 	metrics "github.com/chainguard-dev/terraform-infra-common/pkg/httpmetrics"
 	envConfig "github.com/octo-sts/app/pkg/envconfig"
 	"github.com/octo-sts/app/pkg/ghtransport"
+	"github.com/octo-sts/app/pkg/kms"
 	"github.com/octo-sts/app/pkg/webhook"
 )
 
@@ -45,23 +45,23 @@ func main() {
 		defer metrics.SetupTracer(ctx)()
 	}
 
-	var client *kms.KeyManagementClient
+	var kmsClient kms.KMS
 
 	if baseCfg.KMSKey != "" {
-		client, err = kms.NewKeyManagementClient(ctx)
+		kmsClient, err = kms.NewKMS(ctx, baseCfg.KMSProvider, baseCfg.KMSKey)
 		if err != nil {
 			log.Panicf("could not create kms client: %v", err)
 		}
 	}
 
-	atr, err := ghtransport.New(ctx, baseCfg, client)
+	atr, err := ghtransport.New(ctx, baseCfg, kmsClient)
 	if err != nil {
 		log.Panicf("error creating GitHub App transport: %v", err)
 	}
 
 	// Fetch webhook secrets from secret manager
 	// or allow webhook secret to be defined by env var.
-	// Not everyone is using Google KMS, so we need to support other methods
+	// Not everyone is using Google kmsProvider, so we need to support other methods
 	webhookSecrets := [][]byte{}
 	if baseCfg.KMSKey != "" {
 		secretmanager, err := secretmanager.NewClient(ctx)

pkg/envconfig/envconfig.go
@@ -12,6 +12,7 @@ import (
 type EnvConfig struct {
 	Port                       int    `envconfig:"PORT" required:"true"`
 	KMSKey                     string `envconfig:"KMS_KEY" required:"false"`
+	KMSProvider                string `envconfig:"KMS_PROVIDER" required:"false" default:"gcp"`
 	AppID                      int64  `envconfig:"GITHUB_APP_ID" required:"true"`
 	AppSecretCertificateFile   string `envconfig:"APP_SECRET_CERTIFICATE_FILE" required:"false"`
 	AppSecretCertificateEnvVar string `envconfig:"APP_SECRET_CERTIFICATE_ENV_VAR" required:"false"`

pkg/ghtransport/ghtransport_test.go
@@ -16,8 +16,10 @@ import (
 	"os"
 	"testing"
 
-	kms "cloud.google.com/go/kms/apiv1"
+	gcpKMS "cloud.google.com/go/kms/apiv1"
+	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/octo-sts/app/pkg/envconfig"
+	kmsGCP "github.com/octo-sts/app/pkg/gcpkms"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
@@ -34,13 +36,19 @@ func TestGCPKMS(t *testing.T) {
 	t.Setenv("GOOGLE_APPLICATION_CREDENTIALS", credsFile)
 
 	testConfig := &envconfig.EnvConfig{
-		Port:    8080,
-		AppID:   123456,
-		KMSKey:  "test-kms-key",
-		Metrics: true,
+		Port:        8080,
+		AppID:       123456,
+		KMSKey:      "test-kms-key",
+		KMSProvider: "gcp",
+		Metrics:     true,
+	}
+	provider := testKMSSigner{
+		t:   t,
+		key: testConfig.KMSKey,
+		ctx: ctx,
 	}
 
-	transport, err := New(ctx, testConfig, generateKMSClient(ctx, t))
+	transport, err := New(ctx, testConfig, &provider)
 
 	assert.NoError(t, err)
 
@@ -59,7 +67,7 @@ func TestCertEnvVar(t *testing.T) {
 		Metrics:                    true,
 	}
 
-	transport, err := New(ctx, testConfig, generateKMSClient(ctx, t))
+	transport, err := New(ctx, testConfig, nil)
 
 	assert.NoError(t, err)
 
@@ -76,21 +84,21 @@ func TestCertFile(t *testing.T) {
 		Metrics:                  true,
 	}
 
-	transport, err := New(ctx, testConfig, generateKMSClient(ctx, t))
+	transport, err := New(ctx, testConfig, nil)
 
 	assert.NoError(t, err)
 
 	assert.NotNil(t, transport)
 }
 
-func generateKMSClient(ctx context.Context, t *testing.T) *kms.KeyManagementClient {
+func generateKMSClient(ctx context.Context, t *testing.T) *gcpKMS.KeyManagementClient {
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatal(err)
 	}
 	fakeServerAddr := l.Addr().String()
 
-	client, err := kms.NewKeyManagementClient(ctx,
+	client, err := gcpKMS.NewKeyManagementClient(ctx,
 		option.WithEndpoint(fakeServerAddr),
 		option.WithoutAuthentication(),
 		option.WithGRPCDialOption(grpc.WithTransportCredentials(insecure.NewCredentials())),
@@ -163,4 +171,15 @@ func generateTestCertificateFile(t *testing.T) string {
 	}
 
 	return tmpFile.Name()
+}
+
+type testKMSSigner struct {
+	t   *testing.T
+	key string
+	ctx context.Context
+}
+
+func (k *testKMSSigner) NewSigner() (ghinstallation.Signer, error) {
+	client := generateKMSClient(k.ctx, k.t)
+	return kmsGCP.New(k.ctx, client, k.key)
 }